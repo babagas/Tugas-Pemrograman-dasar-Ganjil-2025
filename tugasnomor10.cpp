812118d3b90d6318eaa2efd3d6a26d1c1330afbe
babagas <brianadrian024@gmail.com>
2025-10-17 11:04:42 +0700

    Add files via upload

---
+++ tugasnomor10.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+union dataDiri{
+int NIU;
+char jenisKelamin;
+int umur;
+double IPK;
+float tinggiBadan;
+};
+dataDiri bagas;
+bagas.NIU = 1234;
+cout << "NIU : " << bagas.NIU << endl;
+bagas.jenisKelamin = 'L';
+cout << "Jenis kelamin : " << (bagas.jenisKelamin == 'L' ? "Laki-laki" : "Perempuan") << endl;
+bagas.umur = 19;
+cout << "Umur : " << bagas.umur << endl;
+bagas.IPK = 3.98;
+cout << "IPK : " << bagas.IPK << endl;
+bagas.tinggiBadan = 170.4;
+cout << "Tinggi badan : " << bagas.tinggiBadan << endl;
+cout << "Ukuran union dataDiri   : " << sizeof(dataDiri) << " byte" << endl;
+cout << "Ukuran member 'NIU'          : " << sizeof(bagas.NIU) << " byte" << endl;
+cout << "Ukuran member 'jenisKelamin'  : " << sizeof(bagas.jenisKelamin) << " byte" << endl;
+cout << "Ukuran member 'umur'          : " << sizeof(bagas.umur) << " byte" << endl;
+cout << "Ukuran member 'IPK'          : " << sizeof(bagas.IPK) << " byte" << endl;
+cout << "Ukuran member 'tinggiBadan'   : " << sizeof(bagas.tinggiBadan) << " byte" << endl;
+return 0 ;
+}
