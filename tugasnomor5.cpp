1c27bc7cda787fcb8896b41a79ea83ca6db17f8f
babagas <220184901+babagas@users.noreply.github.com>
2025-10-17 14:55:04 +0700

    Update tugasnomor5.cpp

--- tugasnomor5.cpp
+++ tugasnomor5.cpp
@@ -2,25 +2,25 @@
 using namespace std;
 int main(){
 int baris = 4;
-    int kolom = 13;
-    char matriks [baris][kolom];
-    for(int i =0;i<baris;i++){
-        for(int j =0;j<kolom;j++){
+int kolom = 13;
+char matriks [baris][kolom];
+for(int i =0;i<baris;i++){
+    for(int j =0;j<kolom;j++){
             matriks[i][j] = ' ';
-        }
     }
-    for(int i=0;i<baris;i++){
-        int jumlahBintang = i*2+1;
-        int posisiBintang = (kolom-(baris+jumlahBintang+1))-1; //dikurangi 1 biar sesuai index
-        for(int j = 0;j<jumlahBintang;j++){
+}
+for(int i=0;i<baris;i++){
+    int jumlahBintang = i*2+1;
+    int posisiBintang = (kolom-(baris+jumlahBintang+1))-1; //dikurangi 1 biar sesuai index
+    for(int j = 0;j<jumlahBintang;j++){
             matriks[i][posisiBintang+(j*2)] = '*';
-        }
     }
-    for(int i=0;i<baris;i++){
-        for(int j=0;j<kolom;j++){
-            cout << matriks[i][j];
-        }
-        cout << endl;
+}
+for(int i=0;i<baris;i++){
+    for(int j=0;j<kolom;j++){
+        cout << matriks[i][j];
     }
-    return 0;
+cout << endl;
+}
+return 0;
 }
