f821bb39136597021c138a453059d8808d64a79e
babagas <brianadrian024@gmail.com>
2025-10-17 13:53:32 +0700

    Update tugasnomor2.cpp

--- tugasnomor2.cpp
+++ tugasnomor2.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int deretBilangan[11];
-    int panjangArray = sizeof(deretBilangan)/sizeof(deretBilangan[0]);
+    int deretBilangan[11]; // elemennya kan 11 berarti index nya sampai 10.
+    int panjangArray = sizeof(deretBilangan)/sizeof(deretBilangan[0]); 
     for(int i=0;i<panjangArray;i++){
-        deretBilangan[i]=3+((i-1)+1)*2;
+        deretBilangan[i]=3+((i+1)-1)*2; // rumus aritmatika a+(n-1)b. pada soal nomer 2 a=3 b=2, i+1 karena pada index ke 10 itu suku ke-11
     }
     for (int j=0;j<panjangArray;j++){
         cout << deretBilangan[j] << " ";
     }
     cout << endl;
     return 0;
+
 }
