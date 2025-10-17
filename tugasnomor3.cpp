729b3b3f2b9b62997439de7efb619fa284a421b9
babagas <brianadrian024@gmail.com>
2025-10-17 13:58:03 +0700

    Update tugasnomor3.cpp

--- tugasnomor3.cpp
+++ tugasnomor3.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int deretBilangan[7];
-    int panjangArray = sizeof(deretBilangan)/sizeof(deretBilangan[0]);
+    int deretBilangan[7]; //banyaknya elemen di array itu 7, berarti indexnya sampai 6
+    int panjangArray = sizeof(deretBilangan)/sizeof(deretBilangan[0]); // ngitung banyaknya elemen di array
     for(int i=0;i<panjangArray;i++){
-        deretBilangan[i]=3+((i-1)+1)*(-1);
+        deretBilangan[i]=3+((i-1)+1)*(-1); // pakai rumus aritmatika a+(n-1).b sama seperti soal nomor 2 dengan a=3 b=-1
     }
     for (int j = 0;j<panjangArray;j++){
         cout << deretBilangan[j] << " "; 
     }
     cout << endl;
     return 0;
+
 }
