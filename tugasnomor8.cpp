3b8c2fe95e21628a7d5b1d720d735ba7b0e0faf2
babagas <brianadrian024@gmail.com>
2025-10-17 14:01:25 +0700

    Update tugasnomor8.cpp

--- tugasnomor8.cpp
+++ tugasnomor8.cpp
@@ -10,8 +10,8 @@ int n;
     cout << "siapa Presiden Pertama Indonesia? Presiden Pertama di Indonesia adalah ... " << endl;
     cout << "clue : ada 8 huruf,huruf pertama wajib kapital" << endl;
     cin >> jawaban;
-    if(strlen(jawaban)==8){
-        if(strcmp(namaPresiden,jawaban)==0){
+    if(strlen(jawaban)==8){ //fungsi strlen
+        if(strcmp(namaPresiden,jawaban)==0){ //fungsi strcmp
             cout << "SELAMAT JAWABANMU BENAR" << endl;
         }else{
         cout << "JAWABAN KAU SALAH" << endl;
@@ -21,7 +21,8 @@ int n;
         cout << "blud tidak bisa baca" << endl;
     }
     
-    cout << strcat(jawabannya,namaPresiden) << endl;
+    cout << strcat(jawabannya,namaPresiden) << endl; //fungsi strcat
     cout << strcat(katajawaban,jawaban) << endl;
     return 0;
+
 }
