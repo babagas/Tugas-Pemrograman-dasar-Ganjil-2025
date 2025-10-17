a5140751fc360be6a164f2c993978e64bddd59db
babagas <brianadrian024@gmail.com>
2025-10-17 13:46:37 +0700

    Update tugasnomor4.cpp

--- tugasnomor4.cpp
+++ tugasnomor4.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(){
-    float v0,h0,h,g = 9.8;
-    int i=0,t = 0;
+    float v0,h0,h,g = 9.8; //v0 = kecepatan awal,h0 = ketinggian awal, h = ketinggian saat t, g = gravitasi
+    int i=0,t = 0; // t = waktu
     cout << "masukkan kecepatan (m/s) : ";
     cin >> v0;
     cout << "masukkan posisi awal dari atas tanah (m) : ";
@@ -29,4 +29,5 @@ int main(){
     }while(h>0);
 }
     return 0;
+
 }
