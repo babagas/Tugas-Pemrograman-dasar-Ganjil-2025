5250af6a575cf367f035831c6f20f0bbb9632321
babagas <brianadrian024@gmail.com>
2025-10-17 14:08:31 +0700

    Update tugasnomor10.cpp

--- tugasnomor10.cpp
+++ tugasnomor10.cpp
@@ -11,7 +11,7 @@ float tinggiBadan;
 };
 dataDiri bagas;
 bagas.NIU = 1234;
-cout << "NIU : " << bagas.NIU << endl;
+cout << "NIU : " << bagas.NIU << endl; 
 bagas.jenisKelamin = 'L';
 cout << "Jenis kelamin : " << (bagas.jenisKelamin == 'L' ? "Laki-laki" : "Perempuan") << endl;
 bagas.umur = 19;
@@ -19,7 +19,7 @@ cout << "Umur : " << bagas.umur << endl;
 bagas.IPK = 3.98;
 cout << "IPK : " << bagas.IPK << endl;
 bagas.tinggiBadan = 170.4;
-cout << "Tinggi badan : " << bagas.tinggiBadan << endl;
+cout << "Tinggi badan : " << bagas.tinggiBadan << endl; //adanya selang-seling member assignment dan cout dikarenakan union cuma punya 1 member yang aktif beda kayak struct yang bisa akses semua member bersamaan
 cout << "Ukuran union dataDiri   : " << sizeof(dataDiri) << " byte" << endl;
 cout << "Ukuran member 'NIU'          : " << sizeof(bagas.NIU) << " byte" << endl;
 cout << "Ukuran member 'jenisKelamin'  : " << sizeof(bagas.jenisKelamin) << " byte" << endl;
@@ -27,4 +27,5 @@ cout << "Ukuran member 'umur'          : " << sizeof(bagas.umur) << " byte" << e
 cout << "Ukuran member 'IPK'          : " << sizeof(bagas.IPK) << " byte" << endl;
 cout << "Ukuran member 'tinggiBadan'   : " << sizeof(bagas.tinggiBadan) << " byte" << endl;
 return 0 ;
+
 }
